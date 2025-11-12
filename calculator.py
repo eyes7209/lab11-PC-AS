527a8450f4e0725c356b11ab30d5984b83a5b394
DaBigCheez3 <Bosstin8@gmail.com>
2025-11-12 10:44:22 -0500

    Modified calculator p1

--- calculator.py
+++ calculator.py
@@ -4,8 +4,35 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
+# First exam
+import math
+def add(a, b):
+    return a+b
     pass
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError('division by zero')
+    return b / a # raise ZeroDivisionError if a == 0
+
+def log(a, b):
+    if a < 0:
+        raise ValueError("Input cannot be a negative number.")
+    return math.log(a,b)
+
+def exp(a, b):
+    return a**b
+
+
+
 
 
